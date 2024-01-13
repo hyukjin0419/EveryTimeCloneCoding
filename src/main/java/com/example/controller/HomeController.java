a30dca88e9bc1fe4c538e27c64a1f4527c3b1b54
LimYeonKyuu <lim000606@naver.com>
2024-01-14 00:36:41 +0900

    init

---
+++ src/main/java/com/example/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @PostMapping("/form")
+    public String form(@RequestParam String name, Model model) {
+        model.addAttribute("name", name);
+
+        return "index";
+    }
+}
---
+++ src/main/java/com/example/entity/Example.java
@@ -0,0 +1,11 @@
+package com.example.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Example {
+    private int id;
+    private String name;
+}
